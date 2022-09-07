5faa0d510b05ee9a52f8e0f150658746033c06fd
Joao Pugliesi <joaopaulo.allex@gmail.com>
2022-09-07 17:53:20 -0300

    Database

---
+++ backend/src/main/java/io/github/joaopugliesi/dsmeta/repository/SaleRepository.java
@@ -0,0 +1,9 @@
+package io.github.joaopugliesi.dsmeta.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import io.github.joaopugliesi.dsmeta.entity.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+
+}
